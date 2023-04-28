95a654a4a964f1bc1ff2d184aa8420643250df35
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-04-28 14:56:48 +0545

    init with added footer

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="absolute bottom-10 left-1/2 -translate-x-1/2">
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
