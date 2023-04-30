b6d190852c6746f8770036b870c101ca8de3a503
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-04-30 11:03:23 +0545

    add scaling instead of width and height to navbar item

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarItem from "./NavbarItem";
 
 function Navbar() {
   return (
-    <div className="flex items-center gap-3">
+    <div className="flex items-center justify-center gap-5 min-w-[100vw] min-h-[100px]">
       {navbarData.map((data, index) => {
         return (
           <NavbarItem key={index} image={data.image} navbarItemIndex={index} />
--- src/components/Navbar/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem.tsx
@@ -9,10 +9,19 @@ interface INavbarItem extends navbarDataType {
 function NavbarItem({ image, navbarItemIndex }: INavbarItem) {
   const { changeNavbarIndex, selctedNavItemIndex } =
     useContext<INavbar>(NavbarContext);
+
+  function isSelected(): boolean {
+    return selctedNavItemIndex === navbarItemIndex;
+  }
+
   return (
     <div
-      className={`flex flex-col items-center justify-center transition-all duration-300 cursor-pointer  bg-slate-300 rounded-xl opacity-80 grayscale hover:grayscale-0 hover:opacity-100  
-      ${selctedNavItemIndex === navbarItemIndex ? "w-16 h-16" : "w-10 h-10"}`}
+      className={`flex flex-col items-center justify-center w-10 h-10 transition-all duration-300 cursor-pointer bg-slate-300 rounded-xl origin-center hover:opacity-100  
+      ${
+        isSelected()
+          ? "scale-150"
+          : "grayscale scale-100 hover:grayscale-0 opacity-80"
+      }`}
       onClick={() => changeNavbarIndex!(navbarItemIndex)}
     >
       <img src={image} alt="" className="w-full h-full rounded-lg" />
