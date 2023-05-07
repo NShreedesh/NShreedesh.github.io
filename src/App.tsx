bf3f62b912c2bfebfe7cc6a81b7051ab5777fb80
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-05-07 21:36:52 +0545

    add grabbing cursor when pressed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import ArrowView from "./components/ArrowView/ArrowView";
 import Navbar from "./components/Navbar/Navbar";
 import ScrollView from "./components/ScrollView/ScrollView";
 
 function App() {
+  const [isGrabbing, setIsGrabbing] = useState<boolean>(false);
+
+  function handleClick(isGrabbing: boolean): void {
+    setIsGrabbing(isGrabbing);
+  }
+
   return (
-    <div className="w-screen h-screen">
+    <div
+      className={`w-screen h-screen ${isGrabbing && "cursor-grabbing"}`}
+      onMouseDown={() => handleClick(true)}
+      onMouseUp={() => handleClick(false)}
+    >
       <Navbar />
       <ArrowView />
       <ScrollView />
