fa5aff09058a45207649d27e06b923f6b995ae74
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-05-07 21:56:58 +0545

    add scroll with mouse

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import ArrowView from "./components/ArrowView/ArrowView";
 import Navbar from "./components/Navbar/Navbar";
 import ScrollView from "./components/ScrollView/ScrollView";
+import { INavbar } from "./context/NavbarContext";
+import NavbarContext from "./context/NavbarContext";
+
+let dragStartPosition: number = 0;
+let dragEndPosition: number = 0;
 
 function App() {
   const [isGrabbing, setIsGrabbing] = useState<boolean>(false);
+  const { selctedNavItemIndex, changeNavbarIndex } =
+    useContext<INavbar>(NavbarContext);
 
-  function handleClick(isGrabbing: boolean): void {
+  function handleMouseDown(
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    isGrabbing: boolean
+  ): void {
+    dragStartPosition = e.clientX;
+    setIsGrabbing(isGrabbing);
+  }
+
+  function handleMouseUp(
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    isGrabbing: boolean
+  ) {
+    dragEndPosition = e.clientX;
+    if (dragEndPosition - dragStartPosition > 0) {
+      changeNavbarIndex?.(selctedNavItemIndex - 1);
+    } else if (dragEndPosition - dragStartPosition < 0) {
+      changeNavbarIndex?.(selctedNavItemIndex + 1);
+    }
+    dragStartPosition = dragEndPosition;
+
     setIsGrabbing(isGrabbing);
   }
 
   return (
     <div
       className={`w-screen h-screen ${isGrabbing && "cursor-grabbing"}`}
-      onMouseDown={() => handleClick(true)}
-      onMouseUp={() => handleClick(false)}
+      onMouseDown={(e) => handleMouseDown(e, true)}
+      onMouseUp={(e) => handleMouseUp(e, false)}
     >
       <Navbar />
       <ArrowView />
