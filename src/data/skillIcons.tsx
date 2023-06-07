c36a95a4edba1038acdf3a3e33ecdda655ce8844
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2023-06-07 12:16:48 +0545

    manage icon size

--- src/data/skillIcons.tsx
+++ src/data/skillIcons.tsx
@@ -19,10 +19,10 @@ export const skillIconsData: IIcons[] = [
     icon: <SiUnity className="text-black dark:text-white" />,
   },
   {
-    icon: <SiCsharp className="text-green-700" />,
+    icon: <SiCsharp className="text-green-700 " />,
   },
   {
-    icon: <SiDotnet className="text-green-700" />,
+    icon: <SiDotnet className="text-5xl text-green-700" />,
   },
   {
     icon: <AiFillHtml5 className="text-red-500" />,
@@ -31,25 +31,25 @@ export const skillIconsData: IIcons[] = [
     icon: <DiCss3 className="text-[#2499e8]" />,
   },
   {
-    icon: <SiTypescript className="text-[#2499e8]" />,
+    icon: <SiTypescript className="text-[#2499e8] text-3xl" />,
   },
   {
-    icon: <DiReact className="text-[#2499e8]" />,
+    icon: <DiReact className="text-[#2499e8] text-5xl" />,
   },
   {
-    icon: <SiFlutter className="text-[#2499e8]" />,
+    icon: <SiFlutter className="text-[#2499e8] text-3xl" />,
   },
   {
     icon: <DiNodejs className="text-green-700 text-7xl" />,
   },
   {
-    icon: <SiExpress className="text-[#2292dd]" />,
+    icon: <SiExpress className="text-[#2292dd] text-3xl" />,
   },
   {
-    icon: <SiXamarin className="text-[#2499e8]" />,
+    icon: <SiXamarin className="text-[#2499e8] text-3xl" />,
   },
   {
-    icon: <SiSass className="text-pink-500" />,
+    icon: <SiSass className="text-3xl text-pink-500" />,
   },
   {
     icon: <SiMysql className="text-[#4ba2dc] text-5xl" />,
